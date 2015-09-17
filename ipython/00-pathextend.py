1cce3fdb1f0bddb4feae5e8636bde47b4ee9fb71
Marouane <marouanefakir@me.com>
2015-09-17 22:23:22 +0100

    Config for may new Mac # Added some config files for other sublime Modules.

--- ipython/00-pathextend.py
+++ ipython/00-pathextend.py
@@ -5,12 +5,10 @@ import sys
 import datetime as dt
 
 # os.chdir('/Users/mar/Desktop/pylab/scripts')
-os.chdir('/Users/mar/PycharmProjects/Medius')
-sys.path.extend(['/Users/mar/PycharmProjects/Medius'])
+os.chdir('/Users/mero/Projects/medius')
+sys.path.append('/Users/mero/Projects/medius')
 
-import Medius
 import pandas as pd
-from PyQt4 import QtGui, QtCore
 from Medius.utils import *
 from Medius.dal.Models import *
-from Medius.lib.Courbe import Courbe
+from Medius.lib.Courbe import CourbeBam
